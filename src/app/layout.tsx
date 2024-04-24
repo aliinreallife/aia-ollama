786367bfa696067993310397073687f07ad2783b
Ali Rashidi <91134550+aliinreallife@users.noreply.github.com>
2024-04-24 12:15:55 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster } from "@/components/ui/sonner"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ollama UI",
-  description: "Ollama chatbot web interface",
+  title: "aia UI",
+  description: "aia chatbot web interface",
 };
 
 export const viewport = {
